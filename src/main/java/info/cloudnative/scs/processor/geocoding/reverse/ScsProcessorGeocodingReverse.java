d8908df24ae6f6e94aa189cb4ec1b39fc760b486
Lei Xu <komushi@gmail.com>
2017-12-22 12:19:25 +0900

    boot app renamed

--- src/main/java/info/cloudnative/scs/processor/geocoding/reverse/ScsProcessorGeocodingReverse.java
+++ src/main/java/info/cloudnative/scs/processor/geocoding/reverse/ScsProcessorGeocodingReverse.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BootApplication {
+public class ScsProcessorGeocodingReverse {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BootApplication.class, args);
+		SpringApplication.run(ScsProcessorGeocodingReverse.class, args);
 	}
 }
